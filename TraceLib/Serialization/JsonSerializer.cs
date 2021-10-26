f57eef70f35da3d12a3a32ab89e33003bd46e8d2
Slizh Anton <antoshka-51762@yandex.by>
2021-10-26 22:34:22 +0300

    Add serialization

---
+++ TraceLib/Serialization/JsonSerializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace TraceLib.Serialization
+{
+    public class JsonSerializer : ISerializer
+    {
+        public string Serialize(TraceResult traceResult)
+        {
+            var data = new Dictionary<string, ICollection<ThreadTrace>>
+            {
+                {"threads", traceResult.GetThreadTraces().Values}
+            };
+            return JsonConvert.SerializeObject(data, Formatting.Indented);
+        }
+    }
+}
---
+++ TraceLib/Serialization/XmlSerializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace TraceLib.Serialization
+{
+    public class XmlSerializer : ISerializer
+    {
+        public string Serialize(TraceResult traceResult)
+        {
+            var data = traceResult.GetThreadTraces().Values.ToArray();
+            var xmlSerializer = new System.Xml.Serialization.XmlSerializer(data.GetType());
+            var stringWriter = new StringWriter();
+            //using(var xmlTextWriter = new System.Xml.Xml)
+            xmlSerializer.Serialize(stringWriter, data);
+            return stringWriter.ToString();
+        }
+    }
+}
