5dea2a10ad4e7d22cab9afb109a23d5dd6409343
Slizh Anton <antoshka-51762@yandex.by>
2021-10-26 20:52:24 +0300

    Add Tracer

---
+++ TraceLib/Tracer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace TraceLib
+{
+    class Tracer : ITracer
+    {
+        private readonly TraceResult _traceResult;
+
+        public Tracer()
+        {
+            _traceResult = new TraceResult();
+        }
+
+        public TraceResult GetTraceResult()
+        {
+            return _traceResult;
+        }
+
+        public void StartTrace()
+        {
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+            ThreadTrace threadTrace = _traceResult.FindThreadTrace(threadId);
+            StackTrace stackTrace = new StackTrace();
+            string[] path = stackTrace.ToString().Split("\r\n");
+            path[0] = "";
+            string methodName = stackTrace.GetFrames()[1].GetMethod().Name;
+            string className = stackTrace.GetFrames()[1].GetMethod().ReflectedType.Name;
+            string methodPath = string.Join("", path);
+            Method method = new Method(methodName, className, methodPath);
+            threadTrace.AddMethod(method);
+        }
+
+        public void StopTrace()
+        {
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+            ThreadTrace threadTrace = _traceResult.FindThreadTrace(threadId);
+            StackTrace stackTrace = new StackTrace();
+            string[] path = stackTrace.ToString().Split("\r\n");
+            path[0] = "";
+            string methodPath = string.Join("", path);
+            threadTrace.RemoveMethod(methodPath);
+        }
+    }
+}
